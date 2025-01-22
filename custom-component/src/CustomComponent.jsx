8b7e713d1b06a04c9609226df70145ed8edde219
Sahaj Jain <sahaj021002@gmail.com>
2025-01-22 18:30:19 +0530

    create npm-package

---
+++ custom-component/src/CustomComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const CustomComponent = ({ title, description }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default CustomComponent;
